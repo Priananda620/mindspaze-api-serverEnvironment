de64ac8896d6c7ac89249fa0deaea36deddf2198
Priananda620 <prianandaazhar2001@gmail.com>
2023-05-05 14:22:04 +0700

    fixed pickle problem

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-import flask_app
+#server
+# import flask_app
+# app = flask_app.app
 
-app = flask_app.app
+
+#local
+from . import create_app
+app = create_app.create_appp()
--- resources/prediction.py
+++ resources/prediction.py
@@ -1,10 +1,41 @@
-import pickle
+# import pickle
 import os
 
 from flask_restful import Resource
 
+######
+from nltk.corpus import wordnet
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer 
+lemmatizer = WordNetLemmatizer()
+stop_words = stopwords.words('english')
+import joblib
+#######
+
 
 class PredictionResource(Resource):
+    
+    def get_wordnet_pos(word):
+        tag = nltk.pos_tag([word])[0][1][0].upper()
+        tag_dict = {"J": wordnet.ADJ,
+                    "N": wordnet.NOUN,
+                    "V": wordnet.VERB,
+                    "R": wordnet.ADV}
+        return tag_dict.get(tag, wordnet.NOUN)
+    
+    def process_text(s):
+        punctuation = string.punctuation+'‘'
+        punctuation = punctuation + '’'
+        nopunc = [char for char in s if char not in punctuation]
+        nopunc = ''.join(nopunc)
+        twitter = ['via', 'photo', 'rt', '@', '#', 'https']
+        stopword = stop_words + twitter
+        clean_string = [word.lower() for word in nopunc.split() if word.lower() not in stopword]
+        clean_string = [lemmatizer.lemmatize(w, PredictionResource.get_wordnet_pos(w)) for w in clean_string]
+        return clean_string
+
     def post(self):
         response = {
             "data": {
@@ -14,23 +45,22 @@ class PredictionResource(Resource):
         }
 
         model_name = os.getcwd()+"/machine_learning/models/svm_countVec_model.sav"
-        # model_file = open(model_name, "rb")
-        # model = pickle.load(model_file)
+        # model_name = "svm_countVec_model.sav"
+        loaded_model = joblib.load(model_name)
+        # loaded_model.named_steps['bow'](analyzer=PredictionResource.process_text)
+
+        article_text = response.get("data").get("comment")
+        article_predict_loaded_model = loaded_model.predict([article_text])
 
 
-        # article_text = response.get("data").get("comment")
-        # article_predict_loaded_model = model.predict([article_text])
+        # f = open(os.getcwd()+"/machine_learning/model/aaa.txt", 'r')
+        # content = f.read()
 
-
-        f = open(os.getcwd()+"/machine_learning/model/aaa.txt", 'r')
-        content = f.read()
-
-        article_predict_loaded_model=True
+        # article_predict_loaded_model=True
 
         data = {
-            "is_hoax": bool(article_predict_loaded_model),
-            "curr_dir": model_name,
-            "text": content
+            "is_hoax": bool(article_predict_loaded_model[0]),
+            "curr_dir": model_name
         }
 
         return data, 200
